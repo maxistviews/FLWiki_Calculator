cd6333ab5dd9d3da256d78b7d897e899cb48ea7d
Maxistviews <17325179+maxistviews@users.noreply.github.com>
2021-08-13 22:12:08 -0400

    add max

---
+++ max.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         FL Wiki Calculator
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://fallenlondon.wiki/wiki/Make_some_money_on_the_side
+// @icon         https://www.google.com/s2/favicons?domain=fallenlondon.wiki
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
